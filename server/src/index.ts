025a64dbacba29b3d94c620fda69a18d79355522
Manan Gulati <manangulati.dev@gmail.com>
2024-11-06 20:38:44 +0530

    server: initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+
+const app = express();
+cors();
+
+const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000;
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Express on Vercel");
+});
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
